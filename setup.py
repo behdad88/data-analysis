6e2370ece446592963bcd4a54770b2c81d766e81
Behdad Karimi <behdad.k7@gmail.com>
2019-05-18 13:58:08 +0430

    setup.py added

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='data_analysis',
+        version='0.0.1',
+        url='www.github.com/benhoff/data-analysis',
+        license='MIT',
+        author='Behdad Karimi',
+        packages=find_packages(),
+        install_requires=['PyQt5',
+                          'pandas',
+                          'sqlalchemy',
+                          'nltk',
+                          'numpy',
+                          'jupyter',
+                          'PyQtChart',
+                          'lda',
+                          'matplotlib',
+                          'scipy',
+                          'tweepy',
+                          'python-twitter'],
+        entry_points={},
+        extras_require={'dev': ['flake8',]},
+)
